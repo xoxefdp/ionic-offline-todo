be8bcd087e82c06d61d083b5e8aaffa13126cf76
José Diaz <xoxefdp@gmail.com>
2018-06-16 16:22:16 +0200

    updated angular dependencies to 5, fix webpackjsonp error putting build/vender.js on index file

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,92 +1,85 @@
-import {Component} from "@angular/core";
-import {NavController, AlertController} from 'ionic-angular';
-import {TodosProvider} from './../../providers/todos/todos';
+import { Component } from "@angular/core";
+import { AlertController } from 'ionic-angular';
+import { TodosProvider } from './../../providers/todos/todos';
 
 @Component({
-	templateUrl: 'home.html'
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-	todos: any;
+  todos: any;
 
-	constructor(private nav: NavController, private todoService: TodosProvider, private alertController: AlertController) {}
+  constructor(private todoService: TodosProvider, private alertController: AlertController) {}
 
-	ionViewWillEnter(){
-		this.todoService.getTodos()
-			.then((data) => {
-				this.todos = data;
-			});
-	}
+  ionViewWillEnter(){
+    this.todoService.getTodos()
+      .then((data) => {
+        this.todos = data;
+      });
+  }
 
-	// ionViewDidEnter(){
-	// 	this.todoService.getTodos()
-	// 		.then((data) => {
-	// 			this.todos = data;
-	// 		});
-	// }
+  createTodo(){
 
-	createTodo(){
+    let prompt = this.alertController.create({
+      title: 'Add',
+      message: 'What do you need to do?',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'put your todo here...'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.todoService.createTodo({title: data.title});
+          }
+        }
+      ]
+    });
 
-		let prompt = this.alertController.create({
-			title: 'Add',
-			message: 'What do you need to do?',
-			inputs: [
-				{
-					name: 'title',
-					placeholder: 'put your todo here...'
-				}
-			],
-			buttons: [
-				{
-					text: 'Cancel'
-				},
-				{
-					text: 'Save',
-					handler: data => {
-						this.todoService.createTodo({title: data.title});
-					}
-				}
-			]
-		});
+    //this.nav.present(prompt);
+    prompt.present();
 
-		//this.nav.present(prompt);
-		prompt.present();
+  }
 
-	}
+  updateTodo(todo){
+    let prompt = this.alertController.create({
+      title: 'Edit',
+      message: 'Change your mind?',
+      inputs: [
+        {
+          name: 'title',
+          value: todo.title
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.todoService.updateTodo({
+              _id: todo._id,
+              _rev: todo._rev,
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
 
-	updateTodo(todo){
-		let prompt = this.alertController.create({
-			title: 'Edit',
-			message: 'Change your mind?',
-			inputs: [
-				{
-					name: 'title',
-					value: todo.title
-				}
-			],
-			buttons: [
-				{
-					text: 'Cancel'
-				},
-				{
-					text: 'Save',
-					handler: data => {
-						this.todoService.updateTodo({
-							_id: todo._id,
-							_rev: todo._rev,
-							title: data.title
-						});
-					}
-				}
-			]
-		});
+    //this.nav.present(prompt);
+    prompt.present();
+  }
 
-		//this.nav.present(prompt);
-		prompt.present();
-	}
-
-	deleteTodo(todo){
-		this.todoService.deleteTodo(todo);
-	}
+  deleteTodo(todo){
+    this.todoService.deleteTodo(todo);
+  }
 
 }
